5e4c158a735eed913a5057902b0374397ad895d1
AustinMahan <amahan0424@gmail.com>
2018-01-03 22:00:53 -0700

    seed

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 async function main() {
   let client = new pg.Client('postgres://localhost:5432/tradingBot');
   await client.connect();
-  await client.query(`create table states(user_id SERIAL, coin varchar, state varchar, last_price decimal)`);
+  await client.query(`CREATE TABLE IF NOT EXISTS states(user_id SERIAL, coin varchar, state varchar, last_price decimal)`);
   await client.end();
 }
 

c8a2350cebe36e7df0d0c9ee5597ae95a3c79a46
AustinMahan <amahan0424@gmail.com>
2018-01-03 21:31:52 -0700

    iffe no no

--- seed.js
+++ seed.js
@@ -1,8 +1,11 @@
-(async function() {
-  const pg = require('pg');
+const pg = require('pg');
+
+async function main() {
   let client = new pg.Client('postgres://localhost:5432/tradingBot');
   await client.connect();
   // await client.query(`create table state(user_id SERIAL, coin varchar, state varchar, last_price decimal)`);
   await client.query(`INSERT INTO states(coin, state) VALUES ('things', 'SELL')`);
   await client.end();
-})();
+}
+
+main();
